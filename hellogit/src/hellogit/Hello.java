f918ec4816bd417f132232c91bba51efda5437df
Kunlun Shen <kunlun_shen@hotmail.com>
2018-06-13 10:52:48 +1000

    first created

---
+++ hellogit/src/hellogit/Hello.java
@@ -0,0 +1,10 @@
+package hellogit;
+
+public class Hello {
+	public static void main(String[] args){
+		String[] names = {"mike","jen"};
+		for (String name : names) {
+			System.out.println("Hello "+ name);
+		}
+	}
+}
