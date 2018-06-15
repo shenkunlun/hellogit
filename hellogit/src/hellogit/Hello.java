78aa5b78f930a5e429034500617f5344d5615600
Kunlun Shen <kunlun_shen@hotmail.com>
2018-06-15 12:29:02 +1000

    added "Hi"

--- hellogit/src/hellogit/Hello.java
+++ hellogit/src/hellogit/Hello.java
@@ -1,10 +1,11 @@
 package hellogit;
 
 public class Hello {
-	public static void main(String[] args){
-		String[] names = {"mike","jen"};
+	public static void main(String[] args) {
+		String[] names = { "mike", "jen" };
 		for (String name : names) {
-			System.out.println("Hello "+ name);
+			System.out.println("Hello " + name +"!");
+			System.out.format("Hi! %s",  name);
 		}
 	}
 }
