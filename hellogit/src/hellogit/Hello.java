91cfd9e5a47dbdf00f7c42280a0f9a3cb4ade8c0
Kunlun Shen <kunlun_shen2@hotmail.com>
2018-06-15 12:32:09 +1000

    Added a person

--- hellogit/src/hellogit/Hello.java
+++ hellogit/src/hellogit/Hello.java
@@ -2,7 +2,7 @@ package hellogit;
 
 public class Hello {
 	public static void main(String[] args){
-		String[] names = {"mike","jen"};
+		String[] names = {"mike","jen","ben"};
 		for (String name : names) {
 			System.out.println("Hello "+ name);
 		}
