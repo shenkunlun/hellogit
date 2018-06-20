57bbb290915b42cb47a2d33b9a49fcac60c4fff2
Kunlun Shen <kunlun_shen@hotmail.com>
2018-06-20 10:38:21 +1000

    a bit change

--- hellogit/src/hellogit/Hello.java
+++ hellogit/src/hellogit/Hello.java
@@ -2,10 +2,10 @@ package hellogit;
 
 public class Hello {
 	public static void main(String[] args) {
-		String[] names = {"mike","jen","ben"};
+		String[] names = {"mic","jen","ben"};
 		for (String name : names) {
 			System.out.println("Hello " + name +"!");
-			System.out.format("Hi! %s",  name);
+			System.out.format("Hi! %s\n",  name);
 		}
 	}
 }
