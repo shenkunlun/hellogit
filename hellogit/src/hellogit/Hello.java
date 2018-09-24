b0244da06a1773a00b2d2f1efece9d50925e26bf
showgegui <923897576@qq.com>
2018-09-24 21:56:43 +0800

    2

--- hellogit/src/hellogit/Hello.java
+++ hellogit/src/hellogit/Hello.java
@@ -2,7 +2,7 @@ package hellogit;
 
 public class Hello {
 	public static void main(String[] args) {
-		String[] names = {"mic","jen","ben","heng"};
+		String[] names = {"mic","jen","ben","sheng"};
 		for (String name : names) {
 			System.out.println("Hello " + name +"!ssss");
 			System.out.format("Hi! %s\n",  name);
